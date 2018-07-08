02b7d4d03fc5d26173a7dca05d4695f02e93c4e7
Hadeel AlHulail <hadeel@Hadeels-MacBook-Pro.local>
2018-07-08 16:12:42 +0300

    Task_3

--- restaurants/views.py
+++ restaurants/views.py
@@ -6,6 +6,20 @@ def welcome(request):
 def restaurant_list(request):
 
     context = {
+        "my_list": [
+            {
+                "restaurant_name":"Nino",
+                "food_type":"Italian",
+            },
+            {
+                "restaurant_name":"Burger King",
+                "food_type":"American",
+            },
+            {
+                "restaurant_name":"Pizza Hut",
+                "food_type":"Fast Food",
+            },
+        ],
 
     }
     return render(request, 'list.html', context)
@@ -13,7 +27,7 @@ def restaurant_list(request):
 
 def restaurant_detail(request):
 
-    context = {
+    context = {'restaurant_name' : 'Lorenzo'
 
     }
     return render(request, 'detail.html', context)
